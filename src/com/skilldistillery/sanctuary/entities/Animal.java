ee45c9c9576d12144a66d82bfb1ce81435075678
George <villegas.george96@gmail.com>
2024-11-04 00:09:25 -0800

    Starting over. Too many edits

--- src/com/skilldistillery/sanctuary/entities/Animal.java
+++ src/com/skilldistillery/sanctuary/entities/Animal.java
@@ -2,19 +2,5 @@ package com.skilldistillery.sanctuary.entities;
 
 public class Animal {
 	private String name;
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void makeNoise() {
-		
-	}
-
-	public void eat(int amountFood) {
-		System.out.println("I am eating " + amountFood + "foods.");
-	}
+	
 }
